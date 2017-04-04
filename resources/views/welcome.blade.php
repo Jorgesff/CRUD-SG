7872da8dda5fba1c49af7e55a140315e65c543bd
Jorgesff <jorgesff@gmail.com>
2017-04-04 19:34:59 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
+        <!-- Styles -->
+
+    </head>
+    <body>
+      <div class="container">
+        <div class="title">
+          <p class="title--lg">Crud do SG</p>
+        </div>
+        <div class="form-crud">
+
+        </div>
+      </div>
+    </body>
+</html>
